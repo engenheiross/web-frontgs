3f68fb73be073ee3de7e37a05de80b59a463e909
Victor <victubou@gmail.com>
2024-11-20 13:52:46 -0300

    Atualizando a página de boas vindas ao usuário ao terminar o login

--- src/routes/Contato/Contato.jsx
+++ src/routes/Contato/Contato.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Contato() {
   return (
     <div className="w-full max-w-[1440px] mx-auto p-8">
-        <h2 className="text-4xl font-bold text-center text-green-700">CONTATOS</h2>
+        <h1 className="text-4xl font-bold text-center text-green-700">CONTATOS</h1>
         <div className="max-w-3xl mx-auto text-center">
           <p className="mb-4 text-gray-600 p-4">TELEFONE - (11) 9999-9999</p>
           <p className="mb-4 text-gray-600">EMAIL - contato@greenwatt.com</p>
--- src/routes/LoginSucesso/LoginSucesso.jsx
+++ src/routes/LoginSucesso/LoginSucesso.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import {Link} from 'react-router-dom';
 
 export default function LoginSucesso() {
   return (
-    <div>Seja Bem vindo à Green WATT 
-        <Link to='/'>Home</Link>
-        <Link to="/sobre">Sobre</Link>
-        <Link to="/solucao">Solução</Link>
+    <div className="w-full max-w-[1440px] mx-auto p-8">
+      <h1 className="text-4xl font-bold text-center text-green-700">Seja Bem-vindo(a) ao seu Painel de Controle!</h1>
+      <div className="max-w-3xl mx-auto text-center">
+        <p className="mb-4 text-gray-600 p-14">Comece a explorar agora e aproveite ao máximo sua experiência com nossa solução!</p>
+      </div>
     </div>
   )
 }
